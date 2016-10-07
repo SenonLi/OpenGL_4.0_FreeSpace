3a088e3d7cad8df3ec6f8a3fae8b819aeb5aec28
SenonLi <senonli6510@gmail.com>
2016-10-07 11:41:54 -0400

    good in release mode

--- vsSenOpenGL/main.cpp
+++ vsSenOpenGL/main.cpp
@@ -44,37 +44,3 @@ int main(int argc, char *argv[])
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//#include <iostream>
-//
-//int main(int argc, char **argv)
-//{
-//	glutInit(&argc, argv);
-//	glutInitWindowSize(640, 480);
-//	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
-//	glutInitContextProfile(GLUT_CORE_PROFILE);
-//	glutCreateWindow("GLUT");
-//	glewExperimental = GL_TRUE;
-//	glewInit();
-//	std::cout << glGetString(GL_VERSION) << std::endl;
-//	getchar();
-//	return 0;
-//}
