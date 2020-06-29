d0b7a92e8f192aa99ed598fd4a7611d63caf8e24
SenonLi <senonli6510@gmail.com>
2020-06-29 02:58:41 -0400

    find out all available projects in vsSenOpenGL

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "LearnOpenGL_GLFW/Sen_07_TextureGLFW.h"
 #include "LearnOpenGL_GLFW/Sen_09_ModelViewProjection.h"
 #include "LearnOpenGL_GLFW/Sen_10_Camera.h"
-#include "LearnOpenGL_GLFW/Sen_21_AssimpMeshModel.h"
+//#include "LearnOpenGL_GLFW/Sen_21_AssimpMeshModel.h"
 #include "LearnOpenGL_GLFW/Sen_22_DepthTest.h"
 #include "LearnOpenGL_GLFW/Sen_23_StencilObjectOutline.h"
 #include "LearnOpenGL_GLFW/Sen_24_BlendingTest.h"
@@ -10,8 +10,8 @@
 #include "LearnOpenGL_GLFW/Sen_262_RearMirror.h"
 #include "LearnOpenGL_GLFW/Sen_263_DebugRearMirror.h"
 #include "LearnOpenGL_GLFW/Sen_30_GeometryShow.h"
-#include "LearnOpenGL_GLFW/Sen_303_NanoSuitExplode.h"
-#include "LearnOpenGL_GLFW/Sen_430_DebugOutput.h"
+//#include "LearnOpenGL_GLFW/Sen_303_NanoSuitExplode.h"
+//#include "LearnOpenGL_GLFW/Sen_430_DebugOutput.h"
 #include "LearnOpenGL_GLFW/Sen_431_LineTextRendering.h"
 #include "Sinks/BreakOut/Sen_47_BlendSprite.h"
 #include "Sinks/BreakOut/Sen_BreakOut.h"
@@ -19,7 +19,7 @@
 
 int main(int argc, char *argv[])
 {
-	Sen_07_TextureGLFW widget;
+	Sen_22_DepthTest widget;
 	widget.ShowWidget();
 
 	return 1;
